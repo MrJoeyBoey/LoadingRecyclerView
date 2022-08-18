2f60428e61e654b55ca2226fe5792f283171c824
MrJoeyBoey <MrJoeyQin@outlook.com>
2022-08-18 15:39:12 +0800

    增加示例

--- app/src/main/java/com/j/loadingrecyclerview/MainActivity.java
+++ app/src/main/java/com/j/loadingrecyclerview/MainActivity.java
@@ -10,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LoadingRecyclerView lrv = findViewById(R.id.lrv);
+
+        findViewById(R.id.btn1).setOnClickListener(v -> {
+            lrv.setStatus(LoadingRecyclerView.LOADING);
+        });
+        findViewById(R.id.btn2).setOnClickListener(v -> {
+            lrv.setStatus(LoadingRecyclerView.LOAD_SUCCESS);
+        });
+        findViewById(R.id.btn3).setOnClickListener(v -> {
+            lrv.setStatus(LoadingRecyclerView.LOAD_FAILED);
+        });
+        findViewById(R.id.btn4).setOnClickListener(v -> {
+            lrv.setStatus(LoadingRecyclerView.NO_DATA);
+        });
+
     }
 }
